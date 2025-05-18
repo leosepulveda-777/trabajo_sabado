e8810ba38908cf505da71e6e33bf90053060b490
leosepulveda-777 <leosepulvedabedoya1@gmail.com>
2025-05-17 19:04:53 -0500

    Add files via upload

---
+++ trabajp sql.sql
@@ -0,0 +1,56 @@
+CREATE TABLE cliente(
+id  INT PRIMARY KEY UNIQUE ,
+nombre VARCHAR(45) NOT NULL, 
+apellido VARCHAR(45) NOT NULL,
+email VARCHAR(45) NOT NULL UNIQUE ,
+telefono VARCHAR(45),
+createdAt DATETIME NOT NULL,
+updateAt DATETIME NOT NULL
+);
+
+CREATE TABLE venta(
+id INT PRIMARY KEY UNIQUE ,
+nombre VARCHAR(45) NOT NULL UNIQUE,
+metodo_pago VARCHAR(45) NOT NULL,
+createdAt DATETIME NOT NULL,
+updateAt DATETIME 
+);
+
+CREATE TABLE empleado(
+id INT PRIMARY KEY UNIQUE,
+ nombre VARCHAR(45) NOT NULL,
+ apellido VARCHAR(45) NOT NULL,
+ cargo  VARCHAR(45) NOT NULL,
+ salario DOUBLE NOT NULL,
+ createdAt DATETIME NOT NULL,
+ updateAt DATETIME 
+ );
+
+
+CREATE TABLE detalle_venta(
+id INT PRIMARY KEY UNIQUE ,
+cantidad INT NOT NULL,
+precio_unitario DOUBLE NOT NULL,
+producto_id INT  PRIMARY KEY UNIQUE 
+);
+
+
+
+CREATE TABLE categoria(
+id INT  PRIMARY KEY UNIQUE,
+nombre VARCHAR(45) NOT NULL,
+descripcion VARCHAR(45),
+createdAt DATETIME NOT NULL,
+updateAt DATETIME 
+);
+
+
+CREATE TABLE  producto(
+id INT PRIMARY KEY UNIQUE ,
+categoria_id  INT PRIMARY KEY UNIQUE ,
+nombre VARCHAR(45) NOT NULL,
+precio DOUBLE NOT NULL,
+cantidad INT NOT NULL,
+createdAt DATETIME NOT NULL,
+updateAt DATETIME
+);
